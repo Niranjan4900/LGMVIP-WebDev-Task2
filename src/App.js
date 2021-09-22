9b5ae59215c0bcfd04895e71f988f4b319a0f402
Niranjan <niranjanpawal4900@gmail.com>
2021-09-22 13:12:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import User from './components/User'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <User />
+      </div>
+    )
+  }
+}
